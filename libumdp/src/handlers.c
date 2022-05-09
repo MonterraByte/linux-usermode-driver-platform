55ce1d4f0d2817108b96ffb66e1740d2c0b31fcd
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2022-05-09 19:16:44 +0100

    Extract code from the echo handler and fix one of its return codes

--- libumdp/src/handlers.c
+++ libumdp/src/handlers.c
@@ -6,27 +6,29 @@
 #include "error.h"
 #include "protocol.h"
 
+static struct nlattr* find_attribute(struct nlattr** attributes, int type) {
+    for (int i = 0; i < UMDP_ATTR_MAX + 1; i++) {
+        struct nlattr* attribute = attributes[i];
+        if (attribute == NULL) {
+            continue;
+        }
+
+        if (nla_type(attribute) == type) {
+            return attribute;
+        }
+    }
+    return NULL;
+}
+
 int umdp_echo_handler(__attribute__((unused)) struct nl_cache_ops* _cache_ops,
     __attribute__((unused)) struct genl_cmd* _cmd, struct genl_info* info, void* arg) {
     if (arg == NULL) {
         print_err("ignored unexpected echo reply\n");
-        return 0;
+        return NL_SKIP;
     }
     char** out = arg;
 
-    struct nlattr* msg_attr = NULL;
-    for (int i = 0; i < UMDP_ATTR_MAX + 1; i++) {
-        struct nlattr* attr = info->attrs[i];
-        if (attr == NULL) {
-            continue;
-        }
-
-        if (attr->nla_type == UMDP_ATTR_MSG) {
-            msg_attr = attr;
-            break;
-        }
-    }
-
+    struct nlattr* msg_attr = find_attribute(info->attrs, UMDP_ATTR_MSG);
     if (msg_attr == NULL) {
         print_err("received echo reply without a message attribute\n");
         return NL_SKIP;
