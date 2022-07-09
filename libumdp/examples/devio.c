94f093f617fcbc1c6c537c53e9bd9e2178e130d5
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2022-07-09 14:47:06 +0100

    Improve example code

--- libumdp/examples/devio.c
+++ libumdp/examples/devio.c
@@ -1,4 +1,4 @@
-#include <limits.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,124 +10,62 @@ typedef enum {
     WRITE,
 } operation;
 
-typedef enum {
-    U8,
-    U16,
-    U32,
-} length;
-
 char* argv0;
 void print_usage() {
-    fprintf(stderr, "USAGE: %s mode length port [value]\nmode: read, write\nlength: 1, 2, 4\n", argv0);
+    fprintf(stderr, "USAGE: %s read port\n       %s write port value\n", argv0, argv0);
     exit(1);
 }
 
-void parse_args(int argc, char* argv[], operation* op, length* len, uint64_t* port, unsigned long* value) {
+void parse_args(int argc, char* argv[], operation* op, uint64_t* port, uint8_t* value) {
     if (argc < 2) {
         print_usage();
     }
 
     if (strcmp(argv[1], "read") == 0) {
         *op = READ;
-        if (argc != 4) {
+        if (argc != 3) {
             print_usage();
         }
     } else if (strcmp(argv[1], "write") == 0) {
         *op = WRITE;
-        if (argc != 5) {
+        if (argc != 4) {
             print_usage();
         }
 
-        // strtoul can't detect 0. Amazing.
-        if (strcmp(argv[4], "0") == 0) {
-            *value = 0;
-        } else {
-            *value = strtoul(argv[4], NULL, 0);
-            if (*value == 0 || *value == ULONG_MAX) {
-                print_usage();
-            }
+        errno = 0;
+        unsigned long value_ul = strtoul(argv[3], NULL, 0);
+        if (errno != 0) {
+            perror("invalid value");
+            exit(1);
+        } else if (value_ul > UINT8_MAX) {
+            fprintf(stderr, "value cannot be larger than %d\n", UINT8_MAX);
+            exit(1);
         }
+        *value = (uint8_t) value_ul;
     } else {
         print_usage();
     }
 
-    switch (strtol(argv[2], NULL, 0)) {
-        case 1:
-            *len = U8;
-            break;
-        case 2:
-            *len = U16;
-            break;
-        case 4:
-            *len = U32;
-            break;
-        default:
-            print_usage();
-    }
-
+    errno = 0;
     *port = strtoul(argv[3], NULL, 0);
-    if (*port == 0 || *port == ULONG_MAX) {
-        print_usage();
+    if (errno != 0) {
+        perror("invalid port value");
+        exit(1);
     }
 }
 
-void devio_read(umdp_connection* connection, uint64_t port, length len) {
-    int ret = -1;
-    switch (len) {
-        case U8: {
-            uint8_t value;
-            ret = umdp_devio_read_u8(connection, port, &value);
-            if (ret == 0) {
-                printf("%u (%x)\n", value, value);
-            }
-            break;
-        }
-        case U16: {
-            uint16_t value;
-            ret = umdp_devio_read_u16(connection, port, &value);
-            if (ret == 0) {
-                printf("%u (%x)\n", value, value);
-            }
-            break;
-        }
-        case U32: {
-            uint32_t value;
-            ret = umdp_devio_read_u32(connection, port, &value);
-            if (ret == 0) {
-                printf("%u (%x)\n", value, value);
-            }
-            break;
-        }
-    }
-
-    if (ret != 0) {
+void devio_read(umdp_connection* connection, uint64_t port) {
+    uint8_t value;
+    int ret = umdp_devio_read_u8(connection, port, &value);
+    if (ret == 0) {
+        printf("%u (%x)\n", value, value);
+    } else {
         fprintf(stderr, "read failed with code %d\n", ret);
     }
 }
 
-void devio_write(umdp_connection* connection, uint64_t port, length len, unsigned long value) {
-    int ret = -1;
-    switch (len) {
-        case U8:
-            if (value > UINT8_MAX) {
-                print_usage();
-            }
-            ret = umdp_devio_write_u8(connection, port, (uint8_t) value);
-            break;
-        case U16:
-            if (value > UINT16_MAX) {
-                print_usage();
-            }
-            ret = umdp_devio_write_u16(connection, port, (uint16_t) value);
-            break;
-        case U32:
-            if (value > UINT32_MAX) {
-                print_usage();
-            }
-            ret = umdp_devio_write_u32(connection, port, (uint32_t) value);
-            break;
-    }
-
+void devio_write(umdp_connection* connection, uint64_t port, uint8_t value) {
+    int ret = umdp_devio_write_u8(connection, port, value);
     if (ret != 0) {
         fprintf(stderr, "write failed with code %d\n", ret);
     }
@@ -137,10 +75,9 @@ int main(int argc, char* argv[]) {
     argv0 = argv[0];
 
     operation op;
-    length len;
     uint64_t port;
-    unsigned long value;
-    parse_args(argc, argv, &op, &len, &port, &value);
+    uint8_t value;
+    parse_args(argc, argv, &op, &port, &value);
 
     umdp_connection* connection = umdp_connect();
     if (connection == NULL) {
@@ -157,10 +94,10 @@ int main(int argc, char* argv[]) {
 
     switch (op) {
         case READ:
-            devio_read(connection, port, len);
+            devio_read(connection, port);
             break;
         case WRITE: {
-            devio_write(connection, port, len, value);
+            devio_write(connection, port, value);
             break;
         }
     }
