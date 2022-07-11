89d4b7911f05ec7713a50e8a30bf5ad828b5100b
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2022-07-12 00:00:37 +0100

    Add interrupts example

---
+++ libumdp/examples/interrupts.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#include <umdp.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+    umdp_connection* connection = umdp_connect();
+    if (connection == NULL) {
+        fprintf(stderr, "umdp_init returned NULL\n");
+        return 1;
+    }
+
+
+    int ret = umdp_interrupt_subscribe(connection, 0);
+    if (ret != 0) {
+        fprintf(stderr, "umdp_interrupt_subscribe returned %d\n", ret);
+        return 1;
+    }
+
+    printf("Subscribed to IRQ 0 successfully\n");
+    sleep(1);
+    uint32_t irq;
+    //for (int i = 0; i < 10; i++) {
+        ret = umdp_receive_interrupt(connection, &irq);
+        if (ret != 0) {
+            fprintf(stderr, "umdp_receive_interrupt returned %d\n", ret);
+            return 1;
+        }
+        printf("IRQ: %d\n", irq);
+    //    sleep(1);
+    //}
+
+    printf("Disconnecting\n");
+    umdp_disconnect(connection);
+    return 0;
+}
